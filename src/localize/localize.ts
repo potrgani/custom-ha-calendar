05415885c56af88681dad3b1bb5c6456a4fac860
potrgani <drluka123456@gmail.com>
2024-06-17 14:31:12 +0200

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -17,7 +17,6 @@ import * as ru from './languages/ru.json';
 import * as sk from './languages/sk.json';
 import * as sl from './languages/sl.json';
 import * as sv from './languages/sv.json';
-
 import { FEATURE_REQUEST } from '../const';
 import { globalData } from '../helpers/globals';
 
@@ -31,6 +30,7 @@ const languages: object = {
 	et,
 	fi,
 	fr,
+	hr,
 	hu,
 	nb,
 	nl,
@@ -39,7 +39,7 @@ const languages: object = {
 	sk,
 	sl,
 	sv,
-	hr,
+	
 };
 
 const DEFAULT_LANG = 'en';
