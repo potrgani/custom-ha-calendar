7894b4c482130b97244ec83116480ea218fde56a
potrgani <drluka123456@gmail.com>
2024-06-17 13:51:07 +0200

    Update localize.ts

--- src/localize/localize.ts
+++ src/localize/localize.ts
@@ -16,7 +16,7 @@ import * as ru from './languages/ru.json';
 import * as sk from './languages/sk.json';
 import * as sl from './languages/sl.json';
 import * as sv from './languages/sv.json';
-import * as hr from './languages/hr.json
+import * as hr from './languages/hr.json';
 import { FEATURE_REQUEST } from '../const';
 import { globalData } from '../helpers/globals';
 
