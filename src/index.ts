79d9fae215e9b772ae8c81edbb8eb4dde096caba
potrgani <drluka123456@gmail.com>
2024-06-17 16:32:28 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,8 @@ import { CSSResultGroup, LitElement, TemplateResult, html } from 'lit';
 import { customElement, property } from 'lit/decorators.js';
 import './locale.dayjs';
 
-import hr from './hr';
 
-dayjs.locale(hr);
+dayjs.locale('hr-hr');
 dayjs.extend(updateLocale);
 dayjs.extend(relativeTime);
 dayjs.extend(isoWeek);
