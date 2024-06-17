989f724a43137b8a226a618317172cd83b7e5fe6
potrgani <drluka123456@gmail.com>
2024-06-17 15:39:34 +0200

    Update event.func.ts

--- src/lib/event.func.ts
+++ src/lib/event.func.ts
@@ -1,4 +1,4 @@
-import dayjs, { extend } from 'dayjs';
+import dayjs from 'dayjs';
 import customParseFormat from 'dayjs/plugin/customParseFormat';
 import isBetween from 'dayjs/plugin/isBetween';
 
