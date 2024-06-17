a8fca6998c45d0d8afef47fc8f9f95da26161867
potrgani <drluka123456@gmail.com>
2024-06-17 16:17:19 +0200

    Update event.func.ts

--- src/lib/event.func.ts
+++ src/lib/event.func.ts
@@ -6,7 +6,7 @@ import CalendarDay from './calendar.class';
 import EventClass from './event.class';
 import sortEvents from '../common/sort_events';
 import { atomicCardConfig } from '../types/config';
-import hr from './hr';
+import hr from '../src/hr';
 
 dayjs.locale(hr);
 dayjs.extend(customParseFormat);
